94179b2e07ccfa0e823f3bf6d5313a5b5d5fec3b
Shubham-yelekar <shubhamyelekar0@gmail.com>
2023-01-09 22:51:23 +0530

    intersection observer add

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const els = document.querySelectorAll('.fade-left');
 
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3
+}
+
+const cb = (entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+     entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active');
+    }
+  });
+}
+
+let observer = new IntersectionObserver(cb, options);
+
+els.forEach(el => {
+  observer.observe(el);
+});
